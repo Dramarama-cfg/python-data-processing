291925f9d85652e0aee0dabdb1fb1b2d8fd62c3f
Sasha <dramarama.main@gmail.com>
2023-06-24 23:38:58 +0200

    mysql_project

---
+++ docker-mysql-python/docker-myslq.sh
@@ -0,0 +1,7 @@
+docker run -d \
+  --name mysql-employees \
+  -p 3306:3306 \
+  -e MYSQL_ROOT_PASSWORD=test123 \
+  -v $PWD/data:/var/lib/mysql \
+  genschsa/mysql-employees
+read -p "Press any key to exit"
